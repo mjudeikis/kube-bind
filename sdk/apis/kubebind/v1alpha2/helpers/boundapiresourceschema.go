e9752ab3a9586f6cd05ebe157389e9cce2b1d748
Mangirdas Judeikis <mangirdas@judeikis.lt>
2025-09-02 19:52:38 +0300

    remove APIResourceSchemas and move to dynamic state.

--- sdk/apis/kubebind/v1alpha2/helpers/boundapiresourceschema.go
+++ sdk/apis/kubebind/v1alpha2/helpers/boundapiresourceschema.go
@@ -17,31 +17,33 @@ limitations under the License.
 package helpers
 
 import (
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	kubebindv1alpha2 "github.com/kube-bind/kube-bind/sdk/apis/kubebind/v1alpha2"
 )
 
-// CRDToBoundAPIResourceSchema converts a CustomResourceDefinition to an BoundAPIResourceSchema.
-func CRDToBoundAPIResourceSchema(crd *apiextensionsv1.CustomResourceDefinition, prefix string) (*kubebindv1alpha2.APIResourceSchema, error) {
+// CRDToBoundSchema converts a CustomResourceDefinition to an BoundSchema.
+func CRDToBoundSchema(crd *apiextensionsv1.CustomResourceDefinition, prefix string) (*kubebindv1alpha2.BoundSchema, error) {
 	name := prefix + "." + crd.Name
 	informerScope := kubebindv1alpha2.NamespacedScope
-	apiResourceSchema := &kubebindv1alpha2.APIResourceSchema{
+	boundSchema := &kubebindv1alpha2.BoundSchema{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: kubebindv1alpha2.SchemeGroupVersion.String(),
-			Kind:       "APIResourceSchema",
+			Kind:       "BoundSchema",
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
 		},
-		Spec: kubebindv1alpha2.APIResourceSchemaSpec{
+		Spec: kubebindv1alpha2.BoundSchemaSpec{
 			InformerScope: informerScope,
-			APIResourceSchemaCRDSpec: kubebindv1alpha2.APIResourceSchemaCRDSpec{
+			APICRDSpec: kubebindv1alpha2.APICRDSpec{
 				Group: crd.Spec.Group,
 				Names: crd.Spec.Names,
 				Scope: crd.Spec.Scope,
@@ -71,7 +73,7 @@ func CRDToBoundAPIResourceSchema(crd *apiextensionsv1.CustomResourceDefinition, 
 			}
 		}
 
-		apiResourceSchema.Spec.Conversion = crConversion
+		boundSchema.Spec.Conversion = crConversion
 	}
 
 	for _, crdVersion := range crd.Spec.Versions {
@@ -88,17 +90,95 @@ func CRDToBoundAPIResourceSchema(crd *apiextensionsv1.CustomResourceDefinition, 
 			if err != nil {
 				return nil, fmt.Errorf("error converting schema for version %q: %w", crdVersion.Name, err)
 			}
-			apiResourceVersion.Schema = kubebindv1alpha2.CRDVersionSchema{
-				OpenAPIV3Schema: runtime.RawExtension{Raw: rawSchema},
-			}
+			apiResourceVersion.Schema = runtime.RawExtension{Raw: rawSchema}
 		}
 
 		if crdVersion.Subresources != nil {
 			apiResourceVersion.Subresources = *crdVersion.Subresources
 		}
 
-		apiResourceSchema.Spec.Versions = append(apiResourceSchema.Spec.Versions, apiResourceVersion)
+		boundSchema.Spec.Versions = append(boundSchema.Spec.Versions, apiResourceVersion)
 	}
 
-	return apiResourceSchema, nil
+	return boundSchema, nil
+}
+
+func UnstructuredToBoundSchema(u unstructured.Unstructured) (*kubebindv1alpha2.BoundSchema, error) {
+	boundSchema := &kubebindv1alpha2.BoundSchema{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.UnstructuredContent(), boundSchema); err != nil {
+		return nil, err
+	}
+	return boundSchema, nil
+}
+
+func BoundSchemasSpecHash(schemas []*kubebindv1alpha2.BoundSchema) string {
+	hash := sha256.New()
+	for _, schema := range schemas {
+		if err := json.NewEncoder(hash).Encode(schema); err != nil {
+			continue
+		}
+	}
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func BoundSchemaToCRD(schema *kubebindv1alpha2.BoundSchema) *apiextensionsv1.CustomResourceDefinition {
+	crd := &apiextensionsv1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apiextensions.k8s.io/v1",
+			Kind:       "CustomResourceDefinition",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: schema.Name,
+		},
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: schema.Spec.Group,
+			Names: schema.Spec.Names,
+			Scope: schema.Spec.Scope,
+		},
+	}
+
+	if schema.Spec.Conversion != nil {
+		crConversion := &apiextensionsv1.CustomResourceConversion{
+			Strategy: apiextensionsv1.ConversionStrategyType(schema.Spec.Conversion.Strategy),
+		}
+
+		if apiextensionsv1.ConversionStrategyType(schema.Spec.Conversion.Strategy) == apiextensionsv1.WebhookConverter && schema.Spec.Conversion.Webhook != nil {
+			crConversion.Webhook = &apiextensionsv1.WebhookConversion{
+				ConversionReviewVersions: schema.Spec.Conversion.Webhook.ConversionReviewVersions,
+			}
+
+			if schema.Spec.Conversion.Webhook.ClientConfig != nil {
+				crConversion.Webhook.ClientConfig = &apiextensionsv1.WebhookClientConfig{
+					URL:      schema.Spec.Conversion.Webhook.ClientConfig.URL,
+					CABundle: schema.Spec.Conversion.Webhook.ClientConfig.CABundle,
+				}
+			}
+		}
+
+		crd.Spec.Conversion = crConversion
+	}
+
+	for _, version := range schema.Spec.Versions {
+		crdVersion := apiextensionsv1.CustomResourceDefinitionVersion{
+			Name:                     version.Name,
+			Served:                   version.Served,
+			Storage:                  version.Storage,
+			Deprecated:               version.Deprecated,
+			DeprecationWarning:       version.DeprecationWarning,
+			AdditionalPrinterColumns: version.AdditionalPrinterColumns,
+			Subresources:             &version.Subresources,
+		}
+		if len(version.Schema.Raw) > 0 {
+			var jsonSchema apiextensionsv1.JSONSchemaProps
+			if err := json.Unmarshal(version.Schema.Raw, &jsonSchema); err == nil {
+				crdVersion.Schema = &apiextensionsv1.CustomResourceValidation{
+					OpenAPIV3Schema: &jsonSchema,
+				}
+			}
+		}
+
+		crd.Spec.Versions = append(crd.Spec.Versions, crdVersion)
+	}
+
+	return crd
 }
